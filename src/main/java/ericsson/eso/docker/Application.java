b8d2cd6a6034f19ac777b031c8fed0bd933c1bbc
redluc81 <luca.rossu81@gmail.com>
2018-02-15 08:33:43 +0000

    clean code

--- src/main/java/ericsson/eso/docker/Application.java
+++ src/main/java/ericsson/eso/docker/Application.java
@@ -12,11 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
